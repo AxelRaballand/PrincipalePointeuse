307bce1646d0728c7d21341d760505c1ae9929fc
Marouane0101 <marwanchokri87@gmail.com>
2021-06-04 16:27:35 +0200

    modif

--- src/model/common/Planning.java
+++ src/model/common/Planning.java
@@ -3,65 +3,22 @@ package model.common;
 import java.time.LocalTime;
 import java.io.*;
 
-/**
- * Class planning which manage the planning of employee
- *  <p>
- * 	A planning is characterized by :
- * <ul>
- * <li> A day
- * <li> A start hour
- * <li> An end hour
- * <li> A type of task 
- * </ul>
- * </p>
- */
-
 public class Planning implements Serializable{
+	
+	private static int counter=0;
+	
+	private int idPlanning;
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -5368343412678213079L;
-
-	/**
-	 * Day in the planning
-	 * <p>
-	 * @see Planning(String day_planning, LocalTime start_Hour, LocalTime end_Hour, String task_Type)
-	 * @see getDayPlanning()
-	 * @see setDayPlanning(String day_planning)
-	 */
 	private String dayPlanning;
 
-	/**
-	 * Hour when an employee start to work
-	 * <p>
-	 * @see Planning(String day_planning, LocalTime start_Hour, LocalTime end_Hour, String task_Type)
-	 * @see getStartHour()
-	 * @see setStartHour(LocalTime start_Hour)
-	 */
 	private LocalTime startHour;
 	
-	/**
-	 * Hour when an employee stop working
-	 * <p>
-	 * @see Planning(String day_planning, LocalTime start_Hour, LocalTime end_Hour, String task_Type)
-	 * @see getEndhour()
-	 * @see setEndHour(LocalTime end_Hour)
-	 */
 	private LocalTime endHour;
 
-	/**
-	 * Task that the employee did everyday
-	 * <p>
-	 * @see Planning(String day_planning, LocalTime start_Hour, LocalTime end_Hour, String task_Type)
-	 * @see getTaskType()
-	 * @see setTaskType(String task_Type)
-	 */
 	private String taskType;
 
 	/**
-	 * Constructor of the class planning
-	 * <p>
+	 * Constructor
 	 * @param day_planning
 	 * @param start_Hour
 	 * @param end_Hour
@@ -69,6 +26,7 @@ public class Planning implements Serializable{
 	 */
 	public Planning(String day_planning, LocalTime start_Hour, LocalTime end_Hour, String task_Type) {
 		super();
+		idPlanning = counter++;
 		dayPlanning = day_planning;
 		startHour = start_Hour;
 		endHour = end_Hour;
@@ -76,74 +34,75 @@ public class Planning implements Serializable{
 	}
 
 	/**
-	 * Getter of dayPlanning
-	 * <p>
+	 * 
+	 * @return idPlanning
+	 */
+	public int getIdPlanning() {
+		return idPlanning;
+	}
+	
+	/**
+	 * 
 	 * @return dayPlanning
 	 */
-	public String getDayPlanning() {
+	public String getJour_planning() {
 		return dayPlanning;
 	}
 
 	/**
-	 * Setter of dayPlanning
-	 * <p>
+	 * 
 	 * @param day_planning
 	 */
-	public void setDayPlanning(String day_planning) {
+	public void setJour_planning(String day_planning) {
 		dayPlanning = day_planning;
 	}
 
 	/**
-	 * Getter of startHour
-	 * <p>
+	 * 
 	 * @return startHour
 	 */
-	public LocalTime getStartHour() {
+	public LocalTime getHeure_Debut() {
 		return startHour;
 	}
 
 	/**
-	 * Setter of startHour
-	 * <p>
+	 * 
 	 * @param start_Hour
 	 */
-	public void setStartHour(LocalTime start_Hour) {
+	public void setHeure_Debut(LocalTime start_Hour) {
 		startHour = start_Hour;
 	}
 
 	/**
-	 * Getter of endHour
-	 * <p>
+	 * 
 	 * @return endHour
 	 */
-	public LocalTime getEndhour() {
+	public LocalTime getHeure_Fin() {
 		return endHour;
 	}
 
 	/**
-	 * Setter of endHour
-	 * <p>
+	 * 
 	 * @param end_Hour
 	 */
-	public void setEndHour(LocalTime end_Hour) {
+	public void setHeure_Fin(LocalTime end_Hour) {
 		endHour = end_Hour;
 	}
 	
 	/**
-	 * Getter of taskType
-	 * <p>
+	 * 
 	 * @return taskType
 	 */
-	public String getTaskType() {
+
+	public String getType_travail() {
 		return taskType;
 	}
 
 	/**
-	 * Setter of taskType
-	 * <p>
+	 * 
 	 * @param task_Type
 	 */
-	public void setTaskType(String task_Type) {
+	public void setType_travail(String task_Type) {
 		taskType = task_Type;
 	}
 
