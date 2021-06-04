100d9799b48f55d765184fd58e23e66dec695bdf
Marouane CHOKRI <58883290+Marouane0101@users.noreply.github.com>
2021-06-04 18:07:37 +0200

    change

--- src/view/principale/ConsultCalendar.java
+++ src/view/principale/ConsultCalendar.java
@@ -34,22 +34,6 @@ public class ConsultCalendar {
 	}
 
 	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ConsultCalendar window = new ConsultCalendar();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
 	 * Create the application.
 	 */
 	public ConsultCalendar() {
