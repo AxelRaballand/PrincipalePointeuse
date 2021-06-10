9e7edf687d9a49210aebe72482c5bf99c4f8a1fb
Marouane0101 <marwanchokri87@gmail.com>
2021-06-10 14:58:12 +0200

    ajout filtrage

--- src/view/principale/HistoricActivity.java
+++ src/view/principale/HistoricActivity.java
@@ -6,7 +6,9 @@ import javax.swing.JFrame;
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
 import java.awt.event.WindowEvent;
+import java.time.LocalDate;
 
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -15,6 +17,7 @@ import javax.swing.table.DefaultTableModel;
 import model.common.Department;
 import model.common.Employee;
 import model.common.Planning;
+import javax.swing.JButton;
 
 
 public class HistoricActivity {
@@ -42,11 +45,13 @@ public class HistoricActivity {
 		lblTitle.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 14));
 		frame.getContentPane().add(lblTitle, BorderLayout.NORTH);
 		
-        frame.addWindowListener(new java.awt.event.WindowAdapter() {
-            public void windowOpened(java.awt.event.WindowEvent evt) {
-                formWindowOpened(evt);
+		JButton btnToday = new JButton("Filtre by Today's history");
+		btnToday.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+            	btnTodayActionPerformed(evt);
             }
         });
+		frame.getContentPane().add(btnToday, BorderLayout.SOUTH);
 		
 		JScrollPane scrollPane = new JScrollPane();
 		frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
@@ -78,33 +83,9 @@ public class HistoricActivity {
 				{null, null, null, null, null, null},
 				{null, null, null, null, null, null},
 				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
-				{null, null, null, null, null, null},
 			},
 			new String[] {
-				"ID", "FirstName", "LastName", "Department", "CheckinIn", "CheckinOut"
+				"ID", "FirstName", "LastName", "Department", "CheckIn", "CheckOut"
 			}
 		) {
 			Class[] columnTypes = new Class[] {
@@ -115,6 +96,86 @@ public class HistoricActivity {
 			}
 		});
 		scrollPane.setViewportView(historicActivityTable);
+		
+        frame.addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                formWindowOpened(evt);
+            }
+        });
+	}
+
+	protected void btnTodayActionPerformed(ActionEvent evt) {
+		
+		historicActivityTable.setModel(new DefaultTableModel(
+				new Object[][] {
+					{null, null, null, null, null, null},
+					{null, null, null, null, null, null},
+					{null, null, null, null, null, null},
+					{null, null, null, null, null, null},
+					{null, null, null, null, null, null},
+					{null, null, null, null, null, null},
+					{null, null, null, null, null, null},
+					{null, null, null, null, null, null},
+					{null, null, null, null, null, null},
+					{null, null, null, null, null, null},
+					{null, null, null, null, null, null},
+					{null, null, null, null, null, null},
+					{null, null, null, null, null, null},
+					{null, null, null, null, null, null},
+					{null, null, null, null, null, null},
+					{null, null, null, null, null, null},
+					{null, null, null, null, null, null},
+					{null, null, null, null, null, null},
+					{null, null, null, null, null, null},
+					{null, null, null, null, null, null},
+					{null, null, null, null, null, null},
+					{null, null, null, null, null, null},
+					{null, null, null, null, null, null},
+					{null, null, null, null, null, null},
+				},
+				new String[] {
+					"ID", "FirstName", "LastName", "Department", "CheckIn", "CheckOut"
+				}
+			) {
+				Class[] columnTypes = new Class[] {
+					Integer.class, String.class, String.class, String.class, String.class, String.class
+				};
+				public Class getColumnClass(int columnIndex) {
+					return columnTypes[columnIndex];
+				}
+			});
+		
+		int i=0,j=0;
+		for(Department depart : JGraphicMainClient.controller.departments)
+		{
+			for(Employee emp : depart.getEmployeeList())
+			{
+					  for(Planning pl : emp.getPlanningList())
+                      {
+						  System.out.println(pl.getToday().toString()+" == " + LocalDate.now().toString()+" =  "+pl.getToday().toString().equals(LocalDate.now().toString()));
+						  if(pl.getToday().toString().equals(LocalDate.now().toString()))
+						  {
+                   	   j = 0;
+                   	historicActivityTable.setValueAt(emp.getIdEmployee(), i, j);
+                          j++;
+                    historicActivityTable.setValueAt(emp.getName(), i, j);
+                          j++;
+                    historicActivityTable.setValueAt(emp.getSurname(), i, j);
+                          j++;
+                    historicActivityTable.setValueAt(emp.getDepartment().getNameDepartment(), i, j);
+                          j++;
+                    historicActivityTable.setValueAt(pl.getHeure_Debut(), i, j);
+                          j++;
+                    historicActivityTable.setValueAt(pl.getHeure_Fin(), i, j);
+                          j++;
+                      i++;
+						  }
+                      }
+            }
+		}
+		
+		
+		
 	}
 
 	protected void formWindowOpened(WindowEvent evt) {
